e1ce1aee0cc03cbe20c6c89d442f0861b7b47367
acrunyon <amber.c.runyon@gmail.com>
2022-02-24 14:37:15 -0700

    update plots for djf precip and pretty model styles

--- Scatterplots.R
+++ Scatterplots.R
@@ -31,12 +31,12 @@ SiteID = "GGCL"
 CFs <- c("MRI-CGCM3.rcp85","MIROC-ESM-CHEM.rcp85")
 # cols = c("#9A9EE5","#E10720")
 col.RCP2 = c("#3030FF","#FFBF00") #medium blue & light orange
-colors2 <- c("#000080","#FA4646") #Navy & light red
+colors2<- c("#9A9EE5","#E10720") 
 
 #Directory and RData file where daily data series is stored
 DataDir = "C:/Users/achildress/DOI/NPS-NRSS-CCRP-FC Science Adaptation - Documents/General/RSS Stuff/Parks/GRCA_CCSP/"
 
-df<-read.csv(paste0(DataDir,"GGCL_scatterplot.csv"))
+df<-read.csv(paste0(DataDir,"GGCL_scatterplot_TmeanF_DJLPrcpIn.csv"))
 df$GCM <- paste(df$Model,df$RCP,sep=".")
 Tavg25 = quantile(df$Tmean_F,0.25)
 Tavg75 = quantile(df$Tmean_F,0.75)
@@ -52,39 +52,15 @@ df$RCP = factor(df$RCP,levels = unique(df$RCP))
 
 head(df)
 Longx<- "annual average temperature (F)"
-Longy<- "annual average precipitation (in)"
-x <- "DeltaTavg"
-y <- "DeltaPr"
-
-# # Shapes
-# dualscatter = ggplot(df, aes(Tmean_F, Precip_in, xmin=Tavg25, xmax=Tavg75, ymin=Pr25, ymax=Pr75,rcp))
-# dualscatter  + geom_text_repel(aes(label=GCM)) +
-#   
-#   geom_point(aes(x=mean(Tmean_F[which(GCM==CFs[1])]), y=mean(Precip_in[which(GCM==CFs[1])])), shape=21, size=9, stroke=2, colour=colors2[1],fill=colors2[1]) +
-#   geom_point(aes(x=mean(Tmean_F[which(GCM==CFs[2])]), y=mean(Precip_in[which(GCM==CFs[2])])), shape=21, size=9, stroke=2, colour=colors2[2],fill=colors2[2]) +
-#   # geom_point(colour="black",size=3.75) +
-#   geom_point(aes(shape=RCP),colour="black",size=4) +
-#   theme(axis.text=element_text(size=18),
-#         axis.title.x=element_text(size=18,vjust=-0.2),
-#         axis.title.y=element_text(size=18,vjust=0.2),
-#         plot.title=element_text(size=18,face="bold",vjust=2,hjust=0.5),
-#         legend.text=element_text(size=18), legend.title=element_text(size=16)) + 
-#   ###
-#   labs(title ="GGCL changes in climate means in 2055 by model run", 
-#        x = paste("Changes in ",Longx,sep=""), # Change
-#        y = paste("Changes in ",Longy,sep="")) + #change
-#   # scale_color_manual(name="Scenarios", values=c("black")) +
-#     scale_shape_manual(name="Emissions",values=c(17,19)) +
-#   # scale_fill_manual(name="Scenarios",values = c("black")) + 
-#   geom_rect(color = "black", alpha=0) +
-#   geom_hline(aes(yintercept=mean(Precip_in)),linetype=2) + #change
-#   geom_vline(aes(xintercept=mean(Tmean_F)),linetype=2) 
+Longy<- "DJF precipitation (in)"
+x <- "TmeanF"
+y <- "PrcpIn_DJF"
 
 # Colors
-dualscatter = ggplot(df, aes(Tmean_F, Precip_in, xmin=Tavg25, xmax=Tavg75, ymin=Pr25, ymax=Pr75,rcp))
+dualscatter = ggplot(df, aes(TmeanF, PrcpIn_DJF))
 dualscatter  +
-  geom_point(aes(x=mean(Tmean_F[which(GCM==CFs[1])]), y=mean(Precip_in[which(GCM==CFs[1])])), shape=21, size=9, stroke=2, colour=colors2[1],fill=colors2[1]) +
-  geom_point(aes(x=mean(Tmean_F[which(GCM==CFs[2])]), y=mean(Precip_in[which(GCM==CFs[2])])), shape=21, size=9, stroke=2, colour=colors2[2],fill=colors2[2]) +
+  geom_point(aes(x=mean(TmeanF[which(GCM==CFs[1])]), y=mean(PrcpIn_DJF[which(GCM==CFs[1])])), shape=21, size=9, stroke=2, colour=colors2[1],fill=colors2[1]) +
+  geom_point(aes(x=mean(TmeanF[which(GCM==CFs[2])]), y=mean(PrcpIn_DJF[which(GCM==CFs[2])])), shape=21, size=9, stroke=2, colour=colors2[2],fill=colors2[2]) +
   geom_point(colour="black",size=4) +
   geom_point(aes(colour=RCP),size=3) +
   geom_text_repel(aes(label=GCM)) +
@@ -97,13 +73,59 @@ dualscatter  +
   labs(title ="GGCL changes in climate means in 2055 by model run", 
        x = paste("Changes in ",Longx,sep=""), # Change
        y = paste("Changes in ",Longy,sep="")) + #change
-  scale_color_manual(name="Emissions", values=col.RCP2) +
+  scale_color_manual(name="Emissions", values=col.RCP2) 
   # scale_shape_manual(name="Emissions",values=c(17,19)) +
   # scale_fill_manual(name="Emissions",values = col.RCP2) +
-  geom_rect(color = "black", alpha=0) +
-  geom_hline(aes(yintercept=mean(Precip_in)),linetype=2) + #change
-  geom_vline(aes(xintercept=mean(Tmean_F)),linetype=2) 
+
+ggsave(paste0(DataDir,"GGCL-Scatter-rcpCol-modelSelect.png"), width = 15, height = 9)
+
+# circled models
+dualscatter = ggplot(df, aes(TmeanF, PrcpIn_DJF))
+dualscatter  + geom_text_repel(aes(label=GCM)) +
+  geom_point(colour="black",size=4) +
+  geom_point(aes(x=mean(TmeanF[which(GCM==CFs[1])]), y=mean(PrcpIn_DJF[which(GCM==CFs[1])])), shape=21, size=22, stroke=3, colour=colors2[1]) +
+  geom_point(aes(x=mean(TmeanF[which(GCM==CFs[2])]), y=mean(PrcpIn_DJF[which(GCM==CFs[2])])), shape=21, size=22, stroke=3, colour=colors2[2]) +
+  geom_point(aes(x=mean(TmeanF[which(GCM==CFs[1])]), y=mean(PrcpIn_DJF[which(GCM==CFs[1])])), shape=20, size=2, colour=colors2[1]) +
+  geom_point(aes(x=mean(TmeanF[which(GCM==CFs[2])]), y=mean(PrcpIn_DJF[which(GCM==CFs[2])])), shape=20, size=2,  colour=colors2[2]) +
+  theme(axis.text=element_text(size=18),
+        axis.title.x=element_text(size=18,vjust=-0.2),
+        axis.title.y=element_text(size=18,vjust=0.2),
+        plot.title=element_text(size=18,face="bold",vjust=2,hjust=0.5),
+        legend.text=element_text(size=18), legend.title=element_text(size=16)) + 
+  ###
+  labs(title ="GGCL changes in climate means in 2055 by model run", 
+       x = paste("Changes in ",Longx,sep=""), # Change
+       y = paste("Changes in ",Longy,sep="")) + #change
+  scale_color_manual(name="Emissions", values=col.RCP2) 
+# scale_shape_manual(name="Emissions",values=c(17,19)) +
+# scale_fill_manual(name="Emissions",values = col.RCP2) +
+
+ggsave(paste0(DataDir,"GGCL-Scatter-modelSelect.png"), width = 15, height = 9)
 
 
-ggsave(paste0(DataDir,SiteID,"-Scatter-",x,"--",y,".png"), width = 15, height = 9)
 
+dualscatter = ggplot(Future_Means, aes(DeltaTavg, DeltaPr*365, xmin=Tavg25, xmax=Tavg75, ymin=Pr25*365, ymax=Pr75*365))
+
+dualscatter  + geom_text_repel(aes(label=GCM)) +
+  geom_point(colour="black",size=4) +
+  geom_point(aes(x=mean(DeltaTavg[which(CF==CFs[1])]), y=mean(365*DeltaPr[which(CF==CFs[1])])), shape=8, size=7, stroke=3, colour=colors2[1]) +
+  geom_point(aes(x=mean(DeltaTavg[which(CF==CFs[2])]), y=mean(365*DeltaPr[which(CF==CFs[2])])), shape=8, size=7, stroke=3, colour=colors2[2]) +
+  geom_point(aes(x=mean(DeltaTavg[which(WB_GCM==CFs[1])]), y=mean(365*DeltaPr[which(WB_GCM==CFs[1])])), shape=21, size=10, stroke=3, colour=colors2[1]) +
+  geom_point(aes(x=mean(DeltaTavg[which(WB_GCM==CFs[2])]), y=mean(365*DeltaPr[which(WB_GCM==CFs[2])])), shape=21, size=10, stroke=3, colour=colors2[2]) +
+  geom_point(aes(x=mean(DeltaTavg[which(WB_GCM==CFs[1])]), y=mean(365*DeltaPr[which(WB_GCM==CFs[1])])), shape=20, size=2,  colour=colors2[1]) +
+  geom_point(aes(x=mean(DeltaTavg[which(WB_GCM==CFs[2])]), y=mean(365*DeltaPr[which(WB_GCM==CFs[2])])), shape=20, size=2,  colour=colors2[2]) +
+  theme(axis.text=element_text(size=18),
+        axis.title.x=element_text(size=18,vjust=-0.2),
+        axis.title.y=element_text(size=18,vjust=0.2),
+        plot.title=element_text(size=18,face="bold",vjust=2,hjust=0.5),
+        legend.text=element_text(size=18), legend.title=element_text(size=16)) + 
+  ###
+  labs(title =paste(SiteID," Changes in climate means in ", Yr, " by GCM run",sep=""), 
+       x = paste("Changes in ",Longx,sep=""), # Change
+       y = paste("Changes in ",Longy,sep="")) + #change
+  scale_color_manual(name="Scenarios", values=c("black")) +
+  # scale_fill_manual(name="Scenarios",values = c("black")) + 
+  theme(legend.position="none") +
+  geom_rect(color = "black", alpha=0) + 
+  geom_hline(aes(yintercept=mean(DeltaPr*365)),linetype=2) + #change
+  geom_vline(aes(xintercept=mean(DeltaTavg)),linetype=2) #change
